00deb59107a2b5a929e67e381d26197628bdf71a
Shubham Chauhan <shubham.chauhan1@gmail.com>
2015-03-28 21:54:18 +0530

    changes to smtp.js enabled for email verification

--- OGV/server/smtp.js
+++ OGV/server/smtp.js
@@ -1,3 +1,7 @@
 Meteor.startup(function () {
-    process.env.MAIL_URL = 'smtp://indrplus:farheen%3Ftnw90@smtp.gmail.com:25';
+	process.env.MAIL_URL='smtp://postmaster%40sandbox28785b639cdb4a9fbc9470bf69566bf9.mailgun.org:ba02351e0d2e483a7351654fbe79e088@smtp.mailgun.org:587'
+
+	Accounts.config({
+		sendVerificationEmail:true
+	})
 });
